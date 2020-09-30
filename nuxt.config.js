94386695db2c181209b08a9e23bfdf18eaf0544e
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-09-30 15:00:42 -0600

    Revert "Added dist directory for serving statically"
    This reverts commit a41a350929d63ccaf02520a5521d030eeac9df62.

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,9 +22,6 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
   ],
-  router: {
-    linkExactActiveClass: 'text-gray-900'
-  },
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
