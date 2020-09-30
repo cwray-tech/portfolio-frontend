a41a350929d63ccaf02520a5521d030eeac9df62
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-09-30 14:58:37 -0600

    Added dist directory for serving statically

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,9 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
   ],
+  router: {
+    linkExactActiveClass: 'text-gray-900'
+  },
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
