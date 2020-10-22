5e42aef7a621db4ee7ffb31df0c3378d0cda5469
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-10-22 17:57:10 -0600

    Removed netlify form added redirect to success

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,29 +1,48 @@
 <template>
   <div>
+    <div v-if="isBot" class="rounded bg-orange-500 text-white text-lg p-4">
+      Great! You have proven to be a bot! I've destroyed the form from the page, and acted like you successfully sent the form.
+    </div>
+    <div v-else>
+    <div v-if="success" class="rounded bg-indigo-500 text-white text-lg p-4">
+      Great! Your message has been sent successfully. I will try to respond
+      quickly.
+    </div>
     <form
+      v-else
+      v-on:submit.prevent="sendMessage"
       name="contact-form"
       class="grid grid-cols-1 gap-y-6"
-      data-netlify="true"
     >
+      <div v-if="errored" class="rounded bg-red-200 text-lg p-4">
+        Bummer, Something went wrong. Did you fill out all of the fields?
+      </div>
       <div>
         <label for="full_name" class="sr-only">Full name*</label>
         <div class="relative rounded-md shadow-sm">
           <input
-            required
-            name="full_name"
-            id="full_name"
             v-model="name"
+            required
+            name="name"
+            id="full_name"
             class="form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150"
             placeholder="Full name*"
           />
 
         </div>
       </div>
+      <div class="hidden">
+            <label class="sr-only"
+              >Don’t fill this out if you're human: </label>
+              <input v-model="bot" name="bot-field" placeholder="This field is only for the robots." class="form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150"
+            />
+      </div>
       <div>
         <label for="email" class="sr-only">Email*</label>
         <div class="relative rounded-md shadow-sm">
           <input
             required
+            v-model="email"
             name="email"
             id="email"
             type="email"
@@ -36,6 +55,7 @@
         <label for="phone" class="sr-only">Phone</label>
         <div class="relative rounded-md shadow-sm">
           <input
+            v-model="phone"
             name="phone"
             id="phone"
             class="form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150"
@@ -48,6 +68,7 @@
         <div class="relative rounded-md shadow-sm">
           <textarea
             required
+            v-model="message"
             name="message"
             id="message"
             rows="4"
@@ -56,25 +77,80 @@
           ></textarea>
         </div>
       </div>
-      <div>
+      <div class="">
         <span class="inline-flex rounded-md shadow-sm">
           <button
             type="submit"
             class="inline-flex justify-center py-3 px-6 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
           >
-            Submit
+            {{ loading ? "Sending Message..." : "Submit" }}
           </button>
         </span>
       </div>
+      <div v-if="loading" class="LoadingNotification rounded bg-orange-500 text-white text-lg p-4">
+        If this takes forever, I am so sorry in advance. My backend is hosted on Heroku for free, and the server has to rebuild if it has been a while since a form has been submitted.
+      </div>
     </form>
-
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   data() {
-    name: null
-  }
-}
+    return {
+      loading: false,
+      success: false,
+      errored: false,
+      isBot: false,
+      name: "",
+      bot: null,
+      email: "",
+      phone: "",
+      message: "",
+    };
+  },
+  methods: {
+    sendMessage() {
+      this.loading = true;
+      if(this.bot != null){
+        this.isBot = true;
+      }
+      else {
+        this.$axios
+        .post("/messages", {
+          name: this.name,
+          email: this.email,
+          phone: this.phone,
+          message: this.message,
+        })
+        .then((response) => {
+          this.success = true;
+          this.errored = false;
+          if(response.status === 200) {
+               this.$router.push({ path : '/contact-form-submitted' });
+            }
+        })
+        .catch(() => {
+          this.errored = true;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+      }
+    },
+  },
+};
 </script>
+<style scoped>
+  .LoadingNotification {
+    animation: 5s appear;
+  }
+  @keyframes appear {
+    0% {
+      opacity: 0;
+    }
+    50%{
+      opacity: 0;
+    }
+  }
+</style>
