b5eadd6f272269515bcd2622e6f7dea675f1a1fc
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-10-30 10:05:21 -0700

    Updated post index now that returned in order

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -13,16 +13,18 @@ export default {
   data() {
     return {
       postPage: {},
-      posts: [],
-    };
+      posts: []
+    }
   },
   async fetch() {
-    this.posts = await this.$axios.$get("/posts?_sort=published_at:desc");
-    this.postPage = await this.$axios.$get("/posts-page");
+    this.posts = await this.$axios.$get('/posts')
+    this.postPage = await this.$axios.$get('/posts-page')
   },
   computed: {
-    title: function(){
-      return this.postPage.title ? this.postPage.title + ' | Software Engineer' : 'Posts from Chris | Software Engineer'
+    title: function() {
+      return this.postPage.title
+        ? this.postPage.title + ' | Software Engineer'
+        : 'Posts from Chris | Software Engineer'
     }
   },
   head() {
@@ -30,23 +32,25 @@ export default {
       title: this.title,
       meta: [
         {
-          hid: "description",
-          name: "description",
-          content: this.postPage.meta_description,
+          hid: 'description',
+          name: 'description',
+          content: this.postPage.meta_description
         },
         {
-          hid: "og:title",
-          property: "og:title",
-          content: this.title,
+          hid: 'og:title',
+          property: 'og:title',
+          content: this.title
         },
         {
-          hid: "og:image",
-          property: "og:image",
-          content: this.postPage.image ? this.postPage.image.url : "https://chriswray.dev/chris-wray-family.jpg",
-        },
-      ],
-    };
+          hid: 'og:image',
+          property: 'og:image',
+          content: this.postPage.image
+            ? this.postPage.image.url
+            : 'https://chriswray.dev/chris-wray-family.jpg'
+        }
+      ]
+    }
   },
-  fetchOnServer: true,
-};
+  fetchOnServer: true
+}
 </script>
