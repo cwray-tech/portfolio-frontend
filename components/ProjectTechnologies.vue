f8d9ddfb9b01b58ce8cc2a2be2802bb6d519f7b4
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-11-02 12:38:27 -0700

    Updated tech links to use margin instead of space

--- components/ProjectTechnologies.vue
+++ components/ProjectTechnologies.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex space-x-4 flex-wrap">
+  <div class="flex flex-wrap">
     <NuxtLink
       :to="'/technologies/' + technology.slug"
       v-for="technology in project.technologies"
       :key="technology.id"
-      class="text-base leading-6 text-indigo-600 dark:text-indigo-700 font-semibold tracking-wide py-1 rounded-lg uppercase hover:text-indigo-500 hover:bg-white transition ease-in-out duration-150"
+      class="text-base mr-4 leading-6 text-indigo-600 dark:text-indigo-700 font-semibold tracking-wide py-1 rounded-lg uppercase hover:text-indigo-500 hover:bg-white transition ease-in-out duration-150"
     >
       {{ technology.name }}
     </NuxtLink>
