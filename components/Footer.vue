9975889374b1f9b6e2070593b1f581110a2ecb78
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-11-30 14:57:26 -0700

    update footer, put links in directory

--- components/Footer.vue
+++ components/Footer.vue
@@ -21,10 +21,39 @@
           </p>
           <div class="mt-8 flex">
             <a
-              href="https://github.com/cwray-tech"
+              href="https://dev.to/cwraytech"
               target="_blank"
               rel="noopener"
               class="text-gray-400 dark:text-gray-100 hover:text-gray-500 dark:text-gray-200"
+            >
+              <span class="sr-only">DEV Profile</span>
+              <svg
+                fill="currentColor"
+                class="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 448 512"
+              >
+                <script
+                  id="krispX"
+                  type="text/javascript"
+                  async="true"
+                  src="chrome-extension://edmmlbmgelmbipapbhcncjblhejknomb/static/js/patch.js"
+                  model8="chrome-extension://edmmlbmgelmbipapbhcncjblhejknomb/models/small_8k.thw"
+                  model16="chrome-extension://edmmlbmgelmbipapbhcncjblhejknomb/models/small_16k.thw"
+                  workletURL="chrome-extension://edmmlbmgelmbipapbhcncjblhejknomb/wasm/krisp-processor.js"
+                />
+                <path
+                  fill="#000000"
+                  d="M120.12 208.29c-3.88-2.9-7.77-4.35-11.65-4.35H91.03v104.47h17.45c3.88 0 7.77-1.45 11.65-4.35 3.88-2.9 5.82-7.25 5.82-13.06v-69.65c-.01-5.8-1.96-10.16-5.83-13.06zM404.1 32H43.9C19.7 32 .06 51.59 0 75.8v360.4C.06 460.41 19.7 480 43.9 480h360.2c24.21 0 43.84-19.59 43.9-43.8V75.8c-.06-24.21-19.7-43.8-43.9-43.8zM154.2 291.19c0 18.81-11.61 47.31-48.36 47.25h-46.4V172.98h47.38c35.44 0 47.36 28.46 47.37 47.28l.01 70.93zm100.68-88.66H201.6v38.42h32.57v29.57H201.6v38.41h53.29v29.57h-62.18c-11.16.29-20.44-8.53-20.72-19.69V193.7c-.27-11.15 8.56-20.41 19.71-20.69h63.19l-.01 29.52zm103.64 115.29c-13.2 30.75-36.85 24.63-47.44 0l-38.53-144.8h32.57l29.71 113.72 29.57-113.72h32.58l-38.46 144.8z"
+                />
+              </svg>
+            </a>
+            <a
+              href="https://github.com/cwray-tech"
+              target="_blank"
+              rel="noopener"
+              class="text-gray-400 dark:text-gray-100 hover:text-gray-500 dark:text-gray-200 ml-3"
             >
               <span class="sr-only">GitHub</span>
               <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
@@ -35,6 +64,7 @@
                 />
               </svg>
             </a>
+
             <a
               href="https://www.linkedin.com/in/christopher-wray/"
               target="_blank"
@@ -62,7 +92,7 @@
           </div>
         </div>
         <div class="mt-12 grid lg:grid-cols-2 gap-8 xl:mt-0 lg:col-span-2">
-          <div class="md:grid md:grid-cols-2 md:gap-8">
+          <div class="grid grid-cols-2 gap-8">
             <div>
               <h4
                 class="text-sm leading-5 font-semibold tracking-wider text-gray-400 dark:text-gray-100 uppercase"
@@ -111,7 +141,7 @@
               </ul>
             </div>
           </div>
-          <div class="md:grid md:grid-cols-2 md:gap-8">
+          <div class="grid grid-cols-2 gap-8">
             <div>
               <h4
                 class="text-sm leading-5 font-semibold tracking-wider text-gray-400 dark:text-gray-100 uppercase"
