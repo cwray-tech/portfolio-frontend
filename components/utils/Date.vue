699c21d89de092578f7614ef17fe038c1ea36cc8
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-12-11 22:50:54 -0700

    add coma to date

--- components/utils/Date.vue
+++ components/utils/Date.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="text-sm text-gray-500">
-    <time datetime="2020-03-16">{{ $dateFns.format(date, 'MMMM d yyyy') }}</time>
+    <time datetime="2020-03-16">{{ $dateFns.format(date, 'MMMM d, yyyy') }}</time>
   </p>
 </template>
 <script>
