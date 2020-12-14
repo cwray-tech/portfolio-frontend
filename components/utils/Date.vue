a17a80a86417217d22fe39417de5a44803ea0340
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-12-14 05:27:07 -0800

    update dark date style

--- components/utils/Date.vue
+++ components/utils/Date.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="text-sm text-gray-500">
+  <p class="text-sm text-gray-500 dark:text-black">
     <time datetime="2020-03-16">{{ $dateFns.format(date, 'MMMM d, yyyy') }}</time>
   </p>
 </template>
