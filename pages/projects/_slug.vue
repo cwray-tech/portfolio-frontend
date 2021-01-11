5b2ad9d6cf5f59318536debbc14422489de120ca
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2021-01-11 09:49:57 -0800

    remove dots from project detail page.

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -2,52 +2,6 @@
   <div>
     <PageHeader :title="project.name" :subtitle="project.introduction" />
     <div class="relative py-16 bg-white dark:bg-indigo-400 overflow-hidden">
-      <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
-        <div class="relative h-full text-lg max-w-prose mx-auto">
-          <svg
-            class="absolute top-12 left-full transform translate-x-32"
-            width="404"
-            height="384"
-            fill="none"
-            viewBox="0 0 404 384"
-          >
-            <defs>
-              <pattern
-                id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
-                x="0"
-                y="0"
-                width="20"
-                height="20"
-                patternUnits="userSpaceOnUse"
-              >
-                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
-          </svg>
-          <svg
-            class="absolute bottom-0 right-full transform -translate-y-0 -translate-x-32"
-            width="404"
-            height="384"
-            fill="none"
-            viewBox="0 0 404 384"
-          >
-            <defs>
-              <pattern
-                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                x="0"
-                y="0"
-                width="20"
-                height="20"
-                patternUnits="userSpaceOnUse"
-              >
-                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-          </svg>
-        </div>
-      </div>
       <div class="relative px-4 sm:px-6 lg:px-8 min-h-screen">
         <div class="text-lg max-w-prose mx-auto mb-6">
           <h1
