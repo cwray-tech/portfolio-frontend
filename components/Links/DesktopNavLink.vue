e35e35dacacff7aa3c0f3498d7197b9e942a80a5
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2021-01-12 08:34:29 -0800

    Add no-prefetching of nav and footer links.

--- components/Links/DesktopNavLink.vue
+++ components/Links/DesktopNavLink.vue
@@ -1,6 +1,7 @@
 <template>
   <NuxtLink
     :to="link"
+    no-prefetch
     class="text-base leading-6 font-medium text-gray-500 dark:text-gray-200 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150"
   >
     {{ text }}
--- components/Links/FooterLink.vue
+++ components/Links/FooterLink.vue
@@ -2,6 +2,7 @@
   <div class="mt-4">
     <NuxtLink
       :to="link"
+      no-prefetch
       class="text-base leading-6 text-gray-500 dark:text-gray-200 hover:text-gray-900"
     >
       {{ text }}
