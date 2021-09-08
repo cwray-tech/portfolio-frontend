4c698efbf7d2d4237185c50095723c29f95cea1c
Christopher Wray <chris@solmediaco.com>
2021-09-08 15:27:21 -0700

    update navlinks

--- components/General/navbar/DesktopNavLinks.vue
+++ components/General/navbar/DesktopNavLinks.vue
@@ -1,37 +1,41 @@
 <template>
-    <nav class="hidden md:flex space-x-10">
-        <DesktopNavLink v-for="navLink in navLinks" :key="navLink.title" :link="navLink.route" :text="navLink.title" />
-    
-    </nav>
+  <nav class="hidden md:flex space-x-10">
+    <DesktopNavLink
+      v-for="navLink in navLinks"
+      :key="navLink.title"
+      :link="navLink.route"
+      :text="navLink.title"
+    />
+  </nav>
 </template>
 
 <script>
 export default {
-    data: function() {
-        return {
-            navLinks: [
-                {
-                    title: "Home",
-                    route: "/"
-                },
-                {
-                    title: "Projects",
-                    route: "/projects"
-                },
-                {
-                    title: "Posts",
-                    route: "/posts"
-                },
-                {
-                    title: "Thoughts",
-                    route: "/thoughts"
-                },
-                {
-                    title: "Technologies",
-                    route: "/technologies"
-                }
-            ]
+  data: function () {
+    return {
+      navLinks: [
+        {
+          title: 'Home',
+          route: '/'
+        },
+        {
+          title: 'Projects',
+          route: '/projects'
+        },
+        {
+          title: 'Posts',
+          route: '/posts'
+        },
+        {
+          title: 'Categories',
+          route: '/categories'
+        },
+        {
+          title: 'Technologies',
+          route: '/technologies'
         }
+      ]
     }
+  }
 }
 </script>
